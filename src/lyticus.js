e308610ee86cba78d2e6ec0a5155056b2f874a01
Bjorn <krols.bj@gmail.com>
2018-10-26 13:01:15 +0200

    Fix ref to self methods

--- src/lyticus.js
+++ src/lyticus.js
@@ -1,54 +1,57 @@
 export default function Lyticus(clientName, clientProvider) {
   switch (clientName) {
     case 'segment':
-      return {
-        page(name) {
-          clientProvider().page(name);
-        },
-        track(type, name, properties) {
-          clientProvider().track(`${type}--${name}`, properties);
-        },
-        addDocumentTracker(eventType, tagNames) {
-          document.addEventListener(eventType, event => {
-            const { target } = event;
-            const { tagName } = target;
-            if (tagNames.includes(tagName)) {
-              let name = null;
-              let properties = null;
-              // Parse id
-              if (target.id) {
-                name = target.id;
+      const page = name => {
+        clientProvider().page(name);
+      };
+      const track = (type, name, properties) => {
+        clientProvider().track(`${type}--${name}`, properties);
+      };
+      const addDocumentTracker = (eventType, tagNames) => {
+        document.addEventListener(eventType, event => {
+          const { target } = event;
+          const { tagName } = target;
+          if (tagNames.includes(tagName)) {
+            let name = null;
+            let properties = null;
+            // Parse id
+            if (target.id) {
+              name = target.id;
+            }
+            // Parse attributes
+            if (target.attributes) {
+              // -- Ignore
+              const ingoreAttribute = target.attributes.getNamedItem(
+                'data-track-ignore'
+              );
+              if (ingoreAttribute) {
+                return;
               }
-              // Parse attributes
-              if (target.attributes) {
-                // -- Ignore
-                const ingoreAttribute = target.attributes.getNamedItem(
-                  'data-track-ignore'
-                );
-                if (ingoreAttribute) {
-                  return;
-                }
-                // -- Name
-                const nameAttribute = target.attributes.getNamedItem(
-                  'data-track-name'
-                );
-                if (nameAttribute) {
-                  name = nameAttribute.value;
-                }
-                // -- Properties
-                const propertiesAttribute = target.attributes.getNamedItem(
-                  'data-track-properties'
-                );
-                if (propertiesAttribute) {
-                  properties = propertiesAttribute.value;
-                }
+              // -- Name
+              const nameAttribute = target.attributes.getNamedItem(
+                'data-track-name'
+              );
+              if (nameAttribute) {
+                name = nameAttribute.value;
               }
-              if (event) {
-                track(eventType, name, properties);
+              // -- Properties
+              const propertiesAttribute = target.attributes.getNamedItem(
+                'data-track-properties'
+              );
+              if (propertiesAttribute) {
+                properties = propertiesAttribute.value;
               }
             }
-          });
-        }
+            if (event) {
+              track(eventType, name, properties);
+            }
+          }
+        });
+      };
+      return {
+        page,
+        track,
+        addDocumentTracker
       };
     default:
       throw new Error('Client not supported');
